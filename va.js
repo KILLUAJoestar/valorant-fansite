21ea3008cafd76bcae588af8b958d9f87c8cd39d
KILLUAJoestar <elias.araujo@estudante.ifgoiano.edu.br>
2025-08-29 19:41:31 -0300

    Add files via upload

---
+++ va.js
@@ -0,0 +1,7 @@
+function mensagemBoasVindas() {
+  alert("🔥 Bem-vindo ao mundo de Valorant! Prepare-se para a batalha! 🔫");
+}
+
+function toggleMenu() {
+  document.querySelector("nav ul").classList.toggle("active");
+}
